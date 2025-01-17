e5641fb3a43c0147f5881a2238fb4e8e344bdc84
Wentao Liu <wentao.liu.zero@outlook.com>
2025-01-17 18:02:09 +0800

    rewriting material balls

--- material-testball/generate_sequence.py
+++ material-testball/generate_sequence.py
@@ -56,14 +56,55 @@ def generate(translate, first_material_name, second_material_name):
 
     return first_object + second_object + third_object
 
+unit_y = 4.5
+unit_x = 3
 
-for token in [\
-    ("-5 0 -5\n", "ShareMaterialCoatedDiffuse", "Stand"),\
-    ("0 0 0\n", "ShareMaterialConductor", "Stand"),\
-    ("5 0 5\n", "ShareMaterialCoatedDiffuse", "Stand"),\
-    ]:
+def generate_coord(x):
+    up   = [-x * unit_x - unit_y, 0, unit_x * x - unit_y]
+    mid  = [-x * unit_x,          0, unit_x * x  ]
+    down = [-x * unit_x + unit_y, 0, unit_x * x + unit_y]
+
+    return [up, mid, down]
+
+diffuse_row = [\
+    (generate_coord(-2.5)[0], "DiffuseAlpha", "GreyStand"),
+    (generate_coord(-2.5)[1], "DiffuseBeta", "GreyStand"),
+    (generate_coord(-2.5)[2], "DiffuseDelta", "GreyStand"),
+]
+
+diffuse_transmission_row = [\
+    (generate_coord(-2)[0], "DiffuseTransmissionAlpha", "GreyStand"),
+    (generate_coord(-2)[1], "DiffuseTransmissionBeta", "GreyStand"),
+    (generate_coord(-2)[2], "DiffuseTransmissionDelta", "GreyStand"),
+]
+
+coated_diffuse_row = [\
+    (generate_coord(-1)[0], "CoatedDiffuseAlpha", "GreyStand"),
+    (generate_coord(-1)[1], "CoatedDiffuseBeta", "GreyStand"),
+    (generate_coord(-1)[2], "CoatedDiffuseDelta", "GreyStand"),
+]
+
+dielectric_row = [\
+    (generate_coord(0)[0], "DielectricAlpha", "RedStand"),
+    (generate_coord(0)[1], "DielectricBeta", "BlueStand"),
+    (generate_coord(0)[2], "DielectricDelta", "GreenStand"),
+]
+
+conductor_row = [\
+    (generate_coord(1)[0], "Copper", "GreyStand"),
+    (generate_coord(1)[1], "Silver", "GreyStand"),
+    (generate_coord(1)[2], "Gold",   "GreyStand"),
+]
+
+coated_conductor_row = [\
+    (generate_coord(2)[0], "CoatedCopper", "GreyStand"),
+    (generate_coord(2)[1], "CoatedSilver", "GreyStand"),
+    (generate_coord(2)[2], "CoatedGold",   "GreyStand"),
+]
+
+
+for token in  diffuse_transmission_row + coated_diffuse_row + dielectric_row + conductor_row + coated_conductor_row:
     coord, first_material, second_material = token
-    translate = "    Translate " + coord
+    x, y, z = coord
+    translate = "    Translate {} {} {}\n".format(x, y, z)
     print(generate(translate, first_material, second_material))
-
-
